c197682ed77065028afcd4fe0f9d8fdd93e9179f
kjhcocomi <981028_@naver.com>
2022-10-31 23:47:39 +0900

    DeadLock

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -5,44 +5,7 @@
 #include <atomic>
 #include <mutex>
 
-vector<int32> v;
-
-// Mutual Exclusive (상호배타적)
-mutex m;
-
-// RAII
-template<typename T> 
-class LockGuard { // == std::lock_guard
-public:
-	LockGuard(T& m) {
-		_mutex = &m;
-		_mutex->lock();
-	}
-	~LockGuard() {
-		_mutex->unlock();
-	}
-private:
-	T* _mutex;
-};
-
-void Push() {
-	for (int32 i = 0; i < 10000; i++) {
-		//m.lock();
-		std::lock_guard<std::mutex> lockGuard(m);
-		v.push_back(i);
-		//m.unlock();
-	}
-}
-
-
-
 int main()
 {
-	std:: thread t1(Push);
-	std:: thread t2(Push);
-
-	t1.join();
-	t2.join();
-
-	cout << v.size();
+	 
 }
